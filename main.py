c5b67ddf8511e6e44d855bc282a8b839086db09b
ayataka555 <ayataka.juveism@gmail.com>
2020-11-13 23:33:57 +0900

    add platform youtube

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from playsound import playsound
 import requests
 import time
+import sys
 from bs4 import BeautifulSoup
 import asyncio
 from pyppeteer.launcher import launch
 
 
-async def get_viewer(browser):
+async def get_viewer(browser, platform, url=None):
   page = await browser.newPage()
-  await page.goto('https://www.mildom.com/10810380',
+  if platform == 'mildom':
+    url = 'https://www.mildom.com/10810380'
+    class_name='.room-anchor-panel__viewer-container>span'
+  if platform == 'youtube':
+    class_name='.view-count'
+  await page.goto(url,
     waitUntil='networkidle0'
   )
-  element = await page.querySelector('.room-anchor-panel__viewer-container>span')
+  element = await page.querySelector(class_name)
   text = await page.evaluate('elm => elm.innerHTML', element)
-  now_viewer = int(text)
+  if platform == "mildom":
+    now_viewer = int(text)
+  if platform == "youtube":
+    now_viewer = text.replace(' 人が視聴中','').replace(',','')
+    now_viewer = int(now_viewer)
   return now_viewer
 
-async def main():
+async def main(platform,url):
   try:
     old = 0
     while(1):
       browser = await launch()
-      new = await get_viewer(browser)
+      new = await get_viewer(browser, platform,url)
       print('viewer:{}'.format(new))
       if (old < new):
         playsound("./sound/famima.wav")
@@ -34,4 +44,8 @@ async def main():
 
 
 if __name__ == '__main__':
-  asyncio.get_event_loop().run_until_complete(main())
+  url = None
+  if len(sys.argv) is 3:
+    platform = sys.argv[1]
+    url = sys.argv[2]
+  asyncio.get_event_loop().run_until_complete(main(platform,url))
