e6e16f240abf7bf94db9547bb8eebf2b0b224f58
ayataka555 <ayataka.juveism@gmail.com>
2020-11-13 23:04:42 +0900

    fix timeout

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from bs4 import BeautifulSoup
 import asyncio
 from pyppeteer.launcher import launch
 
-old_viewer = 0
 
-async def get_viewer():
-  browser = await launch()
+async def get_viewer(browser):
   page = await browser.newPage()
   await page.goto('https://www.mildom.com/10810380',
     waitUntil='networkidle0'
@@ -16,19 +14,24 @@ async def get_viewer():
   element = await page.querySelector('.room-anchor-panel__viewer-container>span')
   text = await page.evaluate('elm => elm.innerHTML', element)
   now_viewer = int(text)
-  await browser.close()
   return now_viewer
 
-
-if __name__ == '__main__':
+async def main():
   try:
     old = 0
     while(1):
-      new = asyncio.get_event_loop().run_until_complete(get_viewer())
+      browser = await launch()
+      new = await get_viewer(browser)
       print('viewer:{}'.format(new))
       if (old < new):
         playsound("./sound/famima.wav")
       old = new
-      time.sleep(20)
+      await browser.close()
+  except RuntimeError:
+    print("run time error")
   except KeyboardInterrupt:
     print("Interrupted by Ctrl + C")
+
+
+if __name__ == '__main__':
+  asyncio.get_event_loop().run_until_complete(main())
